52e77faf05ea23079081aa8949ff073f4a49a6d9
Manoj <manoj.khanwalkar@gmail.com>
2015-12-06 18:21:18 -0500

    Initial commit

---
+++ src/main/java/json/JSONSchemaGenerator.java
@@ -0,0 +1,12 @@
+package json;
+
+/**
+ * Created by mkhanwalkar on 12/6/15.
+ */
+public class JSONSchemaGenerator {
+
+    //TODO - use http://jsonschema.net/#/
+
+    //TODO - use  http://www.jsonschema2pojo.org/
+
+}
