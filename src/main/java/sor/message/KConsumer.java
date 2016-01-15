55ae1a2c5a7018cb55c24493fb255ba5ec46bdad
Manoj <manoj.khanwalkar@gmail.com>
2016-01-14 22:12:27 -0500

    json to xml sample

---
+++ src/main/java/sor/message/KConsumer.java
@@ -0,0 +1,7 @@
+package sor.message;
+
+/**
+ * Created by mkhanwalkar on 1/14/16.
+ */
+public class KConsumer {
+}
---
+++ src/main/java/sor/message/KProducer.java
@@ -0,0 +1,47 @@
+package sor.message;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+
+public class KProducer {
+    final static String TOPIC = "test";
+
+
+    public static void main(String[] argv) throws Exception {
+        Properties properties = new Properties();
+        properties.put("metadata.broker.list","localhost:9092");
+        properties.put("serializer.class","kafka.serializer.StringEncoder");
+        properties.put("bootstrap.servers", "localhost:9092");
+
+        properties.put("zk.connect","127.0.0.1:2181");
+
+        properties.put("group.id", "test");
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("partition.assignment.strategy", "range");
+        properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+       // props.put("metadata.broker.list", "broker1:9092,broker2:9092");
+
+       // ProducerConfig producerConfig = new ProducerConfig(properties);
+        Producer<String,String> producer = new KafkaProducer<String, String>(properties);
+        SimpleDateFormat sdf = new SimpleDateFormat();
+
+
+        ProducerRecord<String, String> message =new ProducerRecord<String, String>(TOPIC,"Test message from java program " + sdf.format(new Date()));
+        producer.send(message);
+
+        Thread.sleep(1000);
+        producer.close();
+    }
+}
