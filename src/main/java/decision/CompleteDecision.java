e045f60f206b5fcd8f50f7fd05b2f8952bcf59e6
Manoj <manoj.khanwalkar@gmail.com>
2016-02-10 15:35:08 -0500

    json to xml sample

--- src/main/java/decision/CompleteDecision.java
+++ src/main/java/decision/CompleteDecision.java
@@ -8,7 +8,15 @@ public class CompleteDecision {
     public static final int MAXMAPPERS = 20 ;
 
 
-    SpecificDecision specificDecision ;   // common across all the applications .
+    public static final int FRAUDNET = 0;
+    public static final int HUNTER = 1;
+    public static final int PRECISEID = 2;
+    public static final int PROVEID = 3;
+    public static final int IDAUTHENTICATE = 4;
+    public static final int BANKWIZARD = 5;
+
+
+    SpecificDecision[]  specificDecision = new SpecificDecision[MAXMAPPERS];   // common across all the applications .
 
     GenericDecision[] genericDecisions = new GenericDecision[MAXMAPPERS];
 
